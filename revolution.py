057d96bad9df06623298cbda7ba3bb66a00348d8
HuanX1 <76062799+HuanX1@users.noreply.github.com>
2021-03-15 00:11:49 +0000

    Added comment
    More description

--- revolution.py
+++ revolution.py
@@ -1,7 +1,7 @@
 import sys
 p = {}
 numbers = [str(x) for x in sys.stdin.read().split()]
-for n in range(1,len(numbers),2):
+for n in range(1,len(numbers),2): #assuming one line has [NAME] [RESTOCK AMOUNT]
     p.update({numbers[n-1] : int(numbers[n])})
 sort_orders = sorted(p.items(), key=lambda x: x[1], reverse=True)
 for i in sort_orders:
