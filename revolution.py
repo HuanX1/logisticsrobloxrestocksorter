101b5e6bfe9d3df934769144cf5a1857cbbba4cb
HuanX1 <76062799+HuanX1@users.noreply.github.com>
2021-03-15 00:10:37 +0000

    Create revolution.py
    Added file + the code

---
+++ revolution.py
@@ -0,0 +1,8 @@
+import sys
+p = {}
+numbers = [str(x) for x in sys.stdin.read().split()]
+for n in range(1,len(numbers),2):
+    p.update({numbers[n-1] : int(numbers[n])})
+sort_orders = sorted(p.items(), key=lambda x: x[1], reverse=True)
+for i in sort_orders:
+    print(i[0], i[1])
